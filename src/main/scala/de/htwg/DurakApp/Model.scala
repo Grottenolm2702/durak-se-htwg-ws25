1de7d8d29f50413c3cf2a1713190f9d9c03e8b4a
Grottenolm2702 <wronny809@gmail.com>
2025-10-29 14:25:46 +0100

    Split into Model and TUI APP

---
+++ src/main/scala/de/htwg/DurakApp/Model.scala
@@ -0,0 +1,24 @@
+package de.htwg.DurakApp
+
+enum Suit:
+  case Hearts, Diamonds, Clubs, Spades
+
+enum Rank(val value: Int):
+  case Six extends Rank(6)
+  case Seven extends Rank(7)
+  case Eight extends Rank(8)
+  case Nine extends Rank(9)
+  case Ten extends Rank(10)
+  case Jack extends Rank(11)
+  case Queen extends Rank(12)
+  case King extends Rank(13)
+  case Ace extends Rank(14)
+
+case class Card(suit: Suit, rank: Rank)
+case class Player(name: String, hand: List[Card] = List())
+case class GameState(
+    players: List[Player],
+    deck: List[Card],
+    table: List[Card] = List(),
+    trump: Suit
+)
