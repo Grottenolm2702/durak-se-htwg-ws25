df403e9de1a51b01de0bed7444d71d62eb3aec5f
NemetzkiBober <mikecraft.guenther@gmail.com>
2025-11-05 13:14:04 +0100

    Durak.scala init

--- src/main/scala/de/htwg/DurakApp/Durak.scala
+++ src/main/scala/de/htwg/DurakApp/Durak.scala
@@ -1,8 +1,20 @@
 package de.htwg.DurakApp
 import de.htwg.DurakApp.RenderTUI
 import de.htwg.DurakApp
-
+import scala.io.StdIn.readLine
 def main(args: Array[String]): Unit =
+    //val game = GameState(playerList, deckWithTrump.drop(10), deckWithTrump.slice(10, 12), trump)
+    val game = init()
+    println(RenderTUI.renderGame(game))
+
+def moveTrump(list : List[Card]) : List[Card] = 
+    list match
+        case head :: tail => tail :+ head
+        case Nil => Nil
+    
+def initDeck() : List[Card] =
+
+
     val deck = (for {
     suit <- Suit.values
     rank <- Rank.values
@@ -12,12 +24,30 @@ def main(args: Array[String]): Unit =
     val shuffledDeck = scala.util.Random.shuffle(deck)
     val trump = shuffledDeck.head.suit
 
-    val deckWithTrump = shuffledDeck.map(card => card.copy(isTrump = card.suit == trump))
+    shuffledDeck.map(card => card.copy(isTrump = card.suit == trump))
+    moveTrump(shuffledDeck)
 
-    val players = List(
-    Player("Alice", deckWithTrump.take(5)),
-    Player("Bob", deckWithTrump.slice(5, 10))
-    )
+def initPlayerList(deck : List[Card]): List[Player] = 
+    println("How many players?")
+    val numPlayers = readLine().toInt
 
-    val game = GameState(players, deckWithTrump.drop(10), deckWithTrump.slice(10, 12), trump)
-    println(RenderTUI.renderGame(game))
+    val players = for (i <- 1 to numPlayers) yield {
+        println(s"Enter name of player $i: ")
+        val name = readLine()
+        Player(name, deck.take(6))
+    }
+    players.toList
+
+def init(): GameState =
+
+    val deck = initDeck()
+    val trump = deck.last.suit
+    // val playerList = List(
+    // Player("Alice", deck.take(5)),
+    // Player("Bob", deck.slice(5, 10))
+    // )
+
+    val playerlist = initPlayerList(deck)
+
+    GameState(playerlist, deck, deck, trump)
+
