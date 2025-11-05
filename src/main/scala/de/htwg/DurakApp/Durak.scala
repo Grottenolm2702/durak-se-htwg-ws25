778bf6bc41086988b786e55c1db2154981710271
NemetzkiBober <mikecraft.guenther@gmail.com>
2025-11-05 13:55:59 +0100

    def init() ready, no tests

--- src/main/scala/de/htwg/DurakApp/Durak.scala
+++ src/main/scala/de/htwg/DurakApp/Durak.scala
@@ -2,11 +2,12 @@ package de.htwg.DurakApp
 import de.htwg.DurakApp.RenderTUI
 import de.htwg.DurakApp
 import scala.io.StdIn.readLine
+import de.htwg.DurakApp.RenderTUI.renderCard
+import de.htwg.DurakApp.RenderTUI.renderHand
 def main(args: Array[String]): Unit =
     //val game = GameState(playerList, deckWithTrump.drop(10), deckWithTrump.slice(10, 12), trump)
     val game = init()
     println(RenderTUI.renderGame(game))
-
 def moveTrump(list : List[Card]) : List[Card] = 
     list match
         case head :: tail => tail :+ head
@@ -24,30 +25,30 @@ def initDeck() : List[Card] =
     val shuffledDeck = scala.util.Random.shuffle(deck)
     val trump = shuffledDeck.head.suit
 
-    shuffledDeck.map(card => card.copy(isTrump = card.suit == trump))
-    moveTrump(shuffledDeck)
+    val trumpShuffledDeck = shuffledDeck.map(card => card.copy(isTrump = card.suit == trump))
+    moveTrump(trumpShuffledDeck)
 
-def initPlayerList(deck : List[Card]): List[Player] = 
+def initPlayerList(deck : List[Card]): (List[Player], List[Card]) = 
     println("How many players?")
     val numPlayers = readLine().toInt
 
-    val players = for (i <- 1 to numPlayers) yield {
-        println(s"Enter name of player $i: ")
-        val name = readLine()
-        Player(name, deck.take(6))
+    val (players, remainingDeck) =(1 to numPlayers).foldLeft((List.empty[Player], deck)) {
+        case ((playerList, currentDeck), i) =>
+            println(s"Enter name of player $i: ")
+            val name = readLine()
+
+            val (playerCards, newDeck) = currentDeck.splitAt(6)
+            val player = Player(name, playerCards)
+            (playerList :+ player, newDeck)
     }
-    players.toList
+
+    (players, remainingDeck)
 
 def init(): GameState =
 
     val deck = initDeck()
     val trump = deck.last.suit
-    // val playerList = List(
-    // Player("Alice", deck.take(5)),
-    // Player("Bob", deck.slice(5, 10))
-    // )
+    val (playerlist, remainingDeck) = initPlayerList(deck)
 
-    val playerlist = initPlayerList(deck)
-
-    GameState(playerlist, deck, deck, trump)
+    GameState(playerlist, remainingDeck, List[Card](), trump)
 
