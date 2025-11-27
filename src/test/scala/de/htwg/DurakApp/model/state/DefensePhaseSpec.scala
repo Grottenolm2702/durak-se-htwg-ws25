ba0085f45b019ec931c4dc383bdca41a6a426a44
Grottenolm2702 <wronny809@gmail.com>
2025-11-27 14:30:55 +0100

    added defense Phase test for missing branch

--- src/test/scala/de/htwg/DurakApp/model/state/DefensePhaseSpec.scala
+++ src/test/scala/de/htwg/DurakApp/model/state/DefensePhaseSpec.scala
@@ -27,11 +27,11 @@ class DefensePhaseSpec extends AnyWordSpec with Matchers {
       val attackCard = Card(Suit.Clubs, Rank.Six)
       val defenseCard = Card(Suit.Clubs, Rank.Seven)
       val player1 = Player("P1", List(Card(Suit.Clubs, Rank.Six)))
-      val player2 = Player("P2", List(defenseCard)) // Defender has beating card
+      val player2 = Player("P2", List(defenseCard))
       val initialGameState = GameState(
         players = List(player1, player2),
         deck = List.empty,
-        table = Map(attackCard -> None), // Attack card on table
+        table = Map(attackCard -> None),
         discardPile = List.empty,
         trumpCard = Card(Suit.Diamonds, Rank.Ace),
         attackerIndex = 0,
@@ -49,7 +49,7 @@ class DefensePhaseSpec extends AnyWordSpec with Matchers {
 
     "not allow a defender to play a non-beating card" in {
       val attackCard = Card(Suit.Clubs, Rank.Six)
-      val defenseCard = Card(Suit.Clubs, Rank.Six) // Non-beating card
+      val defenseCard = Card(Suit.Clubs, Rank.Six)
       val player1 = Player("P1", List(Card(Suit.Clubs, Rank.Eight)))
       val player2 = Player("P2", List(defenseCard))
       val initialGameState = GameState(
@@ -70,7 +70,7 @@ class DefensePhaseSpec extends AnyWordSpec with Matchers {
     "allow a defender to take cards" in {
       val attackCard = Card(Suit.Clubs, Rank.Six)
       val player1 = Player("P1", List(Card(Suit.Clubs, Rank.Six)))
-      val player2 = Player("P2", List(Card(Suit.Hearts, Rank.Seven))) // Defender has some cards
+      val player2 = Player("P2", List(Card(Suit.Hearts, Rank.Seven)))
       val initialGameState = GameState(
         players = List(player1, player2),
         deck = List.empty,
@@ -82,10 +82,10 @@ class DefensePhaseSpec extends AnyWordSpec with Matchers {
         gamePhase = DefensePhase
       )
 
-      val resultState = DefensePhase.takeCards(1, initialGameState) // Defender takes
+      val resultState = DefensePhase.takeCards(1, initialGameState)
       resultState.players(1).hand.should(contain(attackCard))
       resultState.table.should(be (empty))
-      resultState.gamePhase.shouldBe(AttackPhase) // RoundPhase -> AttackPhase after handle
+      resultState.gamePhase.shouldBe(AttackPhase)
       resultState.lastEvent.get.shouldBe(GameEvent.RoundEnd(cleared = false))
     }
 
@@ -104,8 +104,137 @@ class DefensePhaseSpec extends AnyWordSpec with Matchers {
         gamePhase = DefensePhase
       )
 
-      val resultState = DefensePhase.takeCards(0, initialGameState) // Attacker tries to take
+      val resultState = DefensePhase.takeCards(0, initialGameState)
       resultState.shouldBe(initialGameState.copy(lastEvent = Some(GameEvent.NotYourTurn)))
+    }
+
+        "not allow a non-defender to play a card (NotYourTurn)" in {
+      val attackCard = Card(Suit.Clubs, Rank.Six)
+      val defenseCard = Card(Suit.Clubs, Rank.Seven)
+      val attacker = Player("A", List(attackCard))
+      val nonDefender = Player("B", List(defenseCard))
+      val initial = GameState(
+        players = List(attacker, nonDefender),
+        deck = List.empty,
+        table = Map(attackCard -> None),
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Diamonds, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = DefensePhase
+      )
+
+      val res = DefensePhase.playCard(defenseCard, 0, initial)
+      res shouldBe initial.copy(lastEvent = Some(GameEvent.NotYourTurn))
+    }
+
+    "not allow playCard when defender does not have the card (InvalidMove)" in {
+      val attackCard = Card(Suit.Clubs, Rank.Six)
+      val defender = Player("D", List.empty)
+      val attacker = Player("A", List(attackCard))
+      val initial = GameState(
+        players = List(attacker, defender),
+        deck = List.empty,
+        table = Map(attackCard -> None),
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Hearts, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = DefensePhase
+      )
+
+      val attemptedDefense = Card(Suit.Clubs, Rank.Seven)
+      val res = DefensePhase.playCard(attemptedDefense, 1, initial)
+      res shouldBe initial.copy(lastEvent = Some(GameEvent.InvalidMove))
+    }
+
+    "not allow playCard when there is no open attack (attackCardOpt.isEmpty)" in {
+      val attackCard = Card(Suit.Clubs, Rank.Six)
+      val defenseCard = Card(Suit.Clubs, Rank.Seven)
+      val attacker = Player("A", List(attackCard))
+      val defender = Player("D", List(defenseCard))
+      val initial = GameState(
+        players = List(attacker, defender),
+        deck = List.empty,
+        table = Map(attackCard -> Some(Card(Suit.Clubs, Rank.Seven))),
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Hearts, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = DefensePhase
+      )
+
+      val res = DefensePhase.playCard(defenseCard, 1, initial)
+      res shouldBe initial.copy(lastEvent = Some(GameEvent.InvalidMove))
+    }
+
+    "allow defender to use trump to beat a non-trump attack (defenseCard is trump)" in {
+      val attackCard = Card(Suit.Clubs, Rank.Six)
+      val trump = Card(Suit.Hearts, Rank.Seven)
+      val attacker = Player("A", List(attackCard))
+      val defender = Player("D", List(trump))
+      val initial = GameState(
+        players = List(attacker, defender),
+        deck = List.empty,
+        table = Map(attackCard -> None),
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Hearts, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = DefensePhase
+      )
+
+      val res = DefensePhase.playCard(trump, 1, initial)
+      res.players(1).hand.shouldNot(contain(trump))
+      res.table.get(attackCard).flatten.should(contain(trump))
+      res.gamePhase shouldBe AttackPhase
+      res.lastEvent.get.should(be (a[GameEvent.Defend]))
+    }
+
+    "allow defender to beat a trump attack with higher trump (trump vs trump)" in {
+      val attackCard = Card(Suit.Diamonds, Rank.Six)
+      val defenseCard = Card(Suit.Diamonds, Rank.Seven)
+      val attacker = Player("A", List(attackCard))
+      val defender = Player("D", List(defenseCard))
+      val initial = GameState(
+        players = List(attacker, defender),
+        deck = List.empty,
+        table = Map(attackCard -> None),
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Diamonds, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = DefensePhase
+      )
+
+      val res = DefensePhase.playCard(defenseCard, 1, initial)
+      res.players(1).hand.shouldNot(contain(defenseCard))
+      res.table.get(attackCard).flatten.should(contain(defenseCard))
+      res.gamePhase shouldBe AttackPhase
+      res.lastEvent.get.should(be (a[GameEvent.Defend]))
+    }
+
+    "remain in DefensePhase when not all attacks are defended (nextPhase = DefensePhase)" in {
+      val attack1 = Card(Suit.Clubs, Rank.Six)
+      val attack2 = Card(Suit.Clubs, Rank.Seven)
+      val defenseCard = Card(Suit.Clubs, Rank.Eight)
+      val attacker = Player("A", List(attack1, attack2))
+      val defender = Player("D", List(defenseCard))
+      val initial = GameState(
+        players = List(attacker, defender),
+        deck = List.empty,
+        table = Map(attack1 -> None, attack2 -> None),
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Diamonds, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = DefensePhase
+      )
+
+      val res = DefensePhase.playCard(defenseCard, 1, initial)
+      res.gamePhase shouldBe DefensePhase
+      res.table.values.count(_.isEmpty) shouldBe 1
+      res.lastEvent.get.should(be (a[GameEvent.Defend]))
     }
   }
 }
