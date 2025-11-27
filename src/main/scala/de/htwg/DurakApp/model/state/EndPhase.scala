10ae51a0445b2f09cfd4c0b07dc194c4348702bf
Grottenolm2702 <wronny809@gmail.com>
2025-11-27 14:08:42 +0100

    added endphase exception tests

--- src/main/scala/de/htwg/DurakApp/model/state/EndPhase.scala
+++ src/main/scala/de/htwg/DurakApp/model/state/EndPhase.scala
@@ -1,25 +1,36 @@
 package de.htwg.DurakApp.model.state
 
-import de.htwg.DurakApp.model.GameState
+import de.htwg.DurakApp.model._
 
 case object EndPhase extends GamePhase {
+
   override def handle(gameState: GameState): GameState = {
+    require(
+      gameState.players.nonEmpty,
+      "EndPhase: No players in game state. Cannot determine winner."
+    )
+
     val playersWithCards = gameState.players.filter(_.hand.nonEmpty)
     val playersWithoutCards = gameState.players.filter(_.hand.isEmpty)
 
     playersWithCards.headOption match {
+
       case Some(loser) =>
-        val winner = playersWithoutCards.headOption.getOrElse(
-          throw new IllegalStateException("EndPhase: Expected at least one player without cards when a loser is identified.")
+        require(
+          playersWithoutCards.nonEmpty,
+          "EndPhase: Expected at least one player without cards when a loser is identified."
         )
+
+        val winner = playersWithoutCards.head
+
         gameState.copy(
           gamePhase = this,
           lastEvent = Some(GameEvent.GameOver(winner, Some(loser)))
         )
+
       case None =>
-        val representativeWinner = gameState.players.headOption.getOrElse(
-          throw new IllegalStateException("EndPhase: No players in game state. Cannot determine winner.")
-        )
+        val representativeWinner = gameState.players.head
+
         gameState.copy(
           gamePhase = this,
           lastEvent = Some(GameEvent.GameOver(representativeWinner, None))
--- src/test/scala/de/htwg/DurakApp/model/state/EndPhaseSpec.scala
+++ src/test/scala/de/htwg/DurakApp/model/state/EndPhaseSpec.scala
@@ -8,7 +8,8 @@ class EndPhaseSpec extends AnyWordSpec with Matchers {
   "An EndPhase" should {
     "handle a game where there is a clear loser" in {
       val winner = Player("Winner", List.empty, isDone = true)
-      val loser = Player("Loser", List(Card(Suit.Clubs, Rank.Six)), isDone = false)
+      val loser =
+        Player("Loser", List(Card(Suit.Clubs, Rank.Six)), isDone = false)
       val initialGameState = GameState(
         players = List(winner, loser),
         deck = List.empty,
@@ -22,10 +23,17 @@ class EndPhaseSpec extends AnyWordSpec with Matchers {
 
       val resultState = EndPhase.handle(initialGameState)
 
-      resultState.lastEvent.get.should(be (a[GameEvent.GameOver]))
-      resultState.lastEvent.get.asInstanceOf[GameEvent.GameOver].winner.shouldBe(winner)
-      resultState.lastEvent.get.asInstanceOf[GameEvent.GameOver].loser.get.shouldBe(loser)
-      resultState.gamePhase.shouldBe(EndPhase) // Remains in EndPhase
+      resultState.lastEvent.get.should(be(a[GameEvent.GameOver]))
+      resultState.lastEvent.get
+        .asInstanceOf[GameEvent.GameOver]
+        .winner
+        .shouldBe(winner)
+      resultState.lastEvent.get
+        .asInstanceOf[GameEvent.GameOver]
+        .loser
+        .get
+        .shouldBe(loser)
+      resultState.gamePhase.shouldBe(EndPhase)
     }
 
     "handle a game with no clear loser (draw)" in {
@@ -44,9 +52,58 @@ class EndPhaseSpec extends AnyWordSpec with Matchers {
 
       val resultState = EndPhase.handle(initialGameState)
 
-      resultState.lastEvent.get.should(be (a[GameEvent.GameOver]))
-      resultState.lastEvent.get.asInstanceOf[GameEvent.GameOver].loser.shouldBe(None)
+      resultState.lastEvent.get.should(be(a[GameEvent.GameOver]))
+      resultState.lastEvent.get
+        .asInstanceOf[GameEvent.GameOver]
+        .loser
+        .shouldBe(None)
       resultState.gamePhase.shouldBe(EndPhase)
     }
+
+    "throw an exception if a loser exists but no winner can be determined" in {
+      val loser1 =
+        Player("L1", List(Card(Suit.Clubs, Rank.Six)), isDone = false)
+      val loser2 =
+        Player("L2", List(Card(Suit.Hearts, Rank.Seven)), isDone = false)
+
+      val brokenGameState = GameState(
+        players = List(loser1, loser2),
+        deck = List.empty,
+        table = Map.empty,
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Diamonds, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = EndPhase
+      )
+
+      val exception = intercept[IllegalArgumentException] {
+        EndPhase.handle(brokenGameState)
+      }
+
+      exception.getMessage should include
+        "EndPhase: Expected at least one player without cards when a loser is identified."
+    }
+
+    "throw an exception if no players exist in game state" in {
+      val emptyGameState = GameState(
+        players = List.empty,
+        deck = List.empty,
+        table = Map.empty,
+        discardPile = List.empty,
+        trumpCard = Card(Suit.Diamonds, Rank.Ace),
+        attackerIndex = 0,
+        defenderIndex = 1,
+        gamePhase = EndPhase
+      )
+
+      val exception = intercept[IllegalArgumentException] {
+        EndPhase.handle(emptyGameState)
+      }
+
+      exception.getMessage should include
+        "EndPhase: No players in game state. Cannot determine winner."
+    }
+
   }
 }
