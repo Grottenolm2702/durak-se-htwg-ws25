bcb6a5d193596830ea1977f019e9258ca642fbb4
Grottenolm2702 <wronny809@gmail.com>
2025-11-27 12:08:08 +0100

    added tests for Setup and GamePhase

---
+++ src/test/scala/de/htwg/DurakApp/model/state/GamePhaseSpec.scala
@@ -0,0 +1,102 @@
+package de.htwg.DurakApp.model.state
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import de.htwg.DurakApp.model._
+
+class GamePhaseSpec extends AnyWordSpec with Matchers {
+
+  // Test GameEvent case objects and case classes
+  "GameEvent" should {
+    "have correct InvalidMove event" in {
+      GameEvent.InvalidMove shouldBe GameEvent.InvalidMove
+    }
+
+    "have correct NotYourTurn event" in {
+      GameEvent.NotYourTurn shouldBe GameEvent.NotYourTurn
+    }
+
+    "have correct Attack event with card" in {
+      val card = Card(Suit.Clubs, Rank.Ace)
+      val attackEvent = GameEvent.Attack(card)
+      attackEvent.card shouldBe card
+    }
+
+    "have correct Defend event with card" in {
+      val card = Card(Suit.Diamonds, Rank.King)
+      val defendEvent = GameEvent.Defend(card)
+      defendEvent.defenseCard shouldBe card
+    }
+
+    "have correct Pass event" in {
+      GameEvent.Pass shouldBe GameEvent.Pass
+    }
+
+    "have correct Take event" in {
+      GameEvent.Take shouldBe GameEvent.Take
+    }
+
+    "have correct Draw event" in {
+      GameEvent.Draw shouldBe GameEvent.Draw
+    }
+
+    "have correct RoundEnd event with cleared status" in {
+      val roundEndTrue = GameEvent.RoundEnd(true)
+      roundEndTrue.cleared shouldBe true
+
+      val roundEndFalse = GameEvent.RoundEnd(false)
+      roundEndFalse.cleared shouldBe false
+    }
+
+    "have correct GameOver event with winner and optional loser" in {
+      val winner = Player("Winner")
+      val loser = Player("Loser")
+
+      val gameOverWithLoser = GameEvent.GameOver(winner, Some(loser))
+      gameOverWithLoser.winner shouldBe winner
+      gameOverWithLoser.loser shouldBe Some(loser)
+
+      val gameOverNoLoser = GameEvent.GameOver(winner, None)
+      gameOverNoLoser.winner shouldBe winner
+      gameOverNoLoser.loser shouldBe None
+    }
+  }
+
+  // Test default GamePhase trait implementations
+  "GamePhase trait's default methods" should {
+    // Create an anonymous class extending GamePhase for testing default implementations
+    val testGamePhase = new GamePhase {
+      override def handle(gameState: GameState): GameState = gameState // No-op handle for testing defaults
+    }
+
+    val initialGameState = GameState(
+      players = List(Player("P1", List.empty)),
+      deck = List.empty,
+      table = Map.empty,
+      discardPile = List.empty,
+      trumpCard = Card(Suit.Hearts, Rank.Six),
+      attackerIndex = 0,
+      defenderIndex = 0,
+      gamePhase = testGamePhase
+    )
+    val testCard = Card(Suit.Spades, Rank.Seven)
+
+    "return InvalidMove for default playCard" in {
+      val resultState = testGamePhase.playCard(testCard, 0, initialGameState)
+      resultState.lastEvent shouldBe Some(GameEvent.InvalidMove)
+      resultState shouldBe initialGameState.copy(lastEvent = Some(GameEvent.InvalidMove))
+    }
+
+    "return InvalidMove for default pass" in {
+      val resultState = testGamePhase.pass(0, initialGameState)
+      resultState.lastEvent shouldBe Some(GameEvent.InvalidMove)
+      resultState shouldBe initialGameState.copy(lastEvent = Some(GameEvent.InvalidMove))
+    }
+
+    "return InvalidMove for default takeCards" in {
+      val resultState = testGamePhase.takeCards(0, initialGameState)
+      resultState.lastEvent shouldBe Some(GameEvent.InvalidMove)
+      resultState shouldBe initialGameState.copy(lastEvent = Some(GameEvent.InvalidMove))
+    }
+  }
+}
