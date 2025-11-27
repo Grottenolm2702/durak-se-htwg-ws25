feeafe767e67c7bd04be92539c1eac6d1470c693
Grottenolm2702 <wronny809@gmail.com>
2025-11-27 10:56:25 +0100

    Fixed Old Tests

--- src/test/scala/de/htwg/DurakApp/model/CardSpec.scala
+++ src/test/scala/de/htwg/DurakApp/model/CardSpec.scala
@@ -8,8 +8,8 @@ class CardSpec extends AnyWordSpec with Matchers {
   "A Card" should {
     "store its suit, rank and trumpBool correctly" in {
       val card = Card(Suit.Hearts, Rank.Ace, isTrump = true)
-      card.suit shouldBe Suit.Hearts
-      card.rank shouldBe Rank.Ace
+      card.suit.shouldBe(Suit.Hearts)
+      card.rank.shouldBe(Rank.Ace)
     }
   }
 }
