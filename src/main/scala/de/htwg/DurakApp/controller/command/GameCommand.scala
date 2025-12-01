e69efbc63fcc4252060d3ea5a29b188af57f3fdc
Grottenolm2702 <wronny809@gmail.com>
2025-12-01 13:03:00 +0100

    better implemented undo with command

--- src/main/scala/de/htwg/DurakApp/controller/command/GameCommand.scala
+++ src/main/scala/de/htwg/DurakApp/controller/command/GameCommand.scala
@@ -4,4 +4,5 @@ import de.htwg.DurakApp.model.GameState
 
 trait GameCommand {
   def execute(gameState: GameState): GameState
+  def undo(currentGameState: GameState, previousGameState: GameState): GameState = previousGameState
 }
