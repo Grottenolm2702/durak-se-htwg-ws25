a6c4f5f7a7deb6a7c1dc8dca9c528d428353c148
Grottenolm2702 <wronny809@gmail.com>
2025-12-07 16:16:31 +0100

    better main

--- src/main/scala/de/htwg/DurakApp/Main.scala
+++ src/main/scala/de/htwg/DurakApp/Main.scala
@@ -6,16 +6,14 @@ import de.htwg.DurakApp.model.builder.GameStateBuilder
 import de.htwg.DurakApp.util.UndoRedoManager
 
 @main def run: Unit = {
-  val temptui = TUI(
+  val inputTui = TUI(
     new Controller(GameStateBuilder().build(), UndoRedoManager())
   )
-  val deckSize = temptui.askForDeckSize()
-  val playerCount = temptui.askForPlayerCount()
-  val playerNames = temptui.askForPlayerNames(playerCount)
+  val deckSize = inputTui.askForDeckSize()
+  val playerCount = inputTui.askForPlayerCount()
+  val playerNames = inputTui.askForPlayerNames(playerCount)
 
-  val initialGameState = Setup.setupGame(playerNames, deckSize)
-
-  initialGameState.foreach { state =>
+  Setup.setupGame(playerNames, deckSize).foreach { state =>
     val controller = new Controller(state, UndoRedoManager())
     val tui = new TUI(controller)
     controller.add(tui)
